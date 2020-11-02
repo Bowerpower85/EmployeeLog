f0eab5613ac495d78b51b9ac828bc405caf932c0
Jonathan Bower <bowerpower85@gmail.com>
2020-11-02 13:13:14 -0800

    Added a readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# EmployeeLog
+
+  # Project Title : Employee Log
+  ## Project Description:
+  This is a CLI application that allows the client to update their work database with new Departments, Roles, and Employees. They can also view relationships between managers and employees, their respective salaries and more.
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Questions](#questions)
+  * [License](#license)
+  * [Author](#Author)
+  ## Installation
+  Fork the repository and pull down to your local machine. NPM install to ensure you have the dependencies.
+  ## Usage
+  run node server and then you just answer the prompts as they come up.
+  ## Questions
+  If you have any questions, contact Bowerpower85 on GitHub or email at bowerpower85@gmail.com.
+  ## License
+  MIT License 
+  Copyright (c) [year] [Bowerpower85]
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  ## Author
+  ![GitHub profile pic](https://avatars0.githubusercontent.com/u/56975398?v=4)
--- server.js
+++ server.js
@@ -3,194 +3,194 @@ const mysql = require('mysql');
 const consoleTable = require('console.table');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'HappyLittleTrees',
-    database: 'the_company_DB'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'HappyLittleTrees',
+  database: 'the_company_DB'
 });
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log('connected as id ' + connection.threadId);
-    runPrompt();
+  if (err) throw err;
+  console.log('connected as id ' + connection.threadId);
+  runPrompt();
 });
 
 function runPrompt() {
-    inquirer
+  inquirer
     .prompt({
       name: 'create',
       type: 'list',
-      message: 'Create a [Department], a [Role], an [Employee] or [View] the following:',
-            choices: ['Department', 'Role', 'Employee', 'View']
-        })
-        .then(function(promptOptions){
-            if (promptOptions.create === 'Department') {
-                addDepartment();
-            }
-            else if(promptOptions.create === 'Role') {
-                addRole();
-            }
-            else if(promptOptions.create === 'Employee') {
-                addEmployee();
-            }
-            else if(promptOptions.create === 'View') {
-                viewLog();
-            }
-            else{
-                connection.end();
-            }
-        });
+      message: 'Create a [Department], a [Role], an [Employee], or [View] the following:',
+      choices: ['Department', 'Role', 'Employee', 'View']
+    })
+    .then(function (promptOptions) {
+      if (promptOptions.create === 'Department') {
+        addDepartment();
+      }
+      else if (promptOptions.create === 'Role') {
+        addRole();
+      }
+      else if (promptOptions.create === 'Employee') {
+        addEmployee();
+      }
+      else if (promptOptions.create === 'View') {
+        viewLog();
+      }
+      else {
+        connection.end();
+      }
+    });
 }
 
 function addRole() {
-    inquirer
-      .prompt([
-        {
-          name: 'title',
-          type: 'input',
-          message: 'What role are you adding?',
-        },
-        {
-          name: 'salary',
-          type: 'input',
-          message: 'What is the salary for this role?',
-        },
-        {
-          name: 'departmentId',
-          type: 'input',
-          message: 'What departmentID belongs to this role?',
-        }
-      ])
-      .then(function(answer) {
+  inquirer
+    .prompt([
+      {
+        name: 'title',
+        type: 'input',
+        message: 'What role are you adding?',
+      },
+      {
+        name: 'salary',
+        type: 'input',
+        message: 'What is the salary for this role?',
+      },
+      {
+        name: 'departmentId',
+        type: 'input',
+        message: 'What departmentID belongs to this role?',
+      }
+    ])
+    .then(function (answer) {
 
-        connection.query(
-          'INSERT INTO role SET ?',
-          {
-            title: answer.title,
-            salary: answer.salary,
-            department_id: answer.departmentId
-          },
-          function(err) {
-            if (err) throw err;
-            console.log('Role Added!');
-            runPrompt();
-          }
-        );
-      });
-  }
-
-  function addDepartment() {
-    inquirer
-      .prompt([
+      connection.query(
+        'INSERT INTO role SET ?',
         {
-          name: 'department',
-          type: 'input',
-          message: 'What department are you adding?',
-        }
-      ])
-      .then(function(answer) {
-        connection.query(
-          'INSERT INTO department SET ?',
-          {
-            name: answer.department
-          },
-          function(err) {
-            if (err) throw err;
-            console.log('Your department was added!');
-            runPrompt();
-          }
-        );
-      });
-  }
-
-  function addEmployee() {
-    inquirer
-      .prompt([
-        {
-          name: 'first',
-          type: 'input',
-          message: 'What is the first name of the employee?',
+          title: answer.title,
+          salary: answer.salary,
+          department_id: answer.departmentId
         },
-        {
-          name: 'last',
-          type: 'input',
-          message: 'What is the last name of the employee?',
-        },
-        {
-          name: 'roleId',
-          type: 'input',
-          message: 'What role Id number does the employee have?'
-        },
-        {
-          name: 'managerId',
-          type: 'input',
-          message: 'What is the manager ID number for the employee? If none write 0.',
+        function (err) {
+          if (err) throw err;
+          console.log('Role Added!');
+          runPrompt();
         }
-      ])
-      .then(function(answer) {
-        connection.query(
-          'INSERT INTO employee SET ?',
-          {
-            first_name: answer.first,
-            last_name: answer.last,
-            role_id: answer.roleId,
-            manager_id: answer.managerId
-          },
-          function(err) {
-            if (err) throw err;
-            console.log('Employee Added!');
-            runPrompt();
-          }
-        );
-      });
-  }
+      );
+    });
+}
 
-  function viewLog() {
-    inquirer
-      .prompt({
-        name: 'table',
-        type: 'list',
-        message: 'What would you like to review?',
-        choices: ['Department', 'Role', 'Employee']
-      })
-      .then(function(answer) {
-        if (answer.table === 'Department') {
-          viewDepartment();
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: 'department',
+        type: 'input',
+        message: 'What department are you adding?',
+      }
+    ])
+    .then(function (answer) {
+      connection.query(
+        'INSERT INTO department SET ?',
+        {
+          name: answer.department
+        },
+        function (err) {
+          if (err) throw err;
+          console.log('Your department was added!');
+          runPrompt();
         }
-        else if(answer.table === 'Role') {
-          viewRole();
-        } 
-        else if(answer.table === 'Employee') {
-          viewEmployee();
-        }
-        else{
-          connection.end();
-        }
-      });
-  }
+      );
+    });
+}
 
-  function viewRole() {
-    connection.query('SELECT * FROM role', function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      runPrompt();
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: 'first',
+        type: 'input',
+        message: 'What is the first name of the employee?',
+      },
+      {
+        name: 'last',
+        type: 'input',
+        message: 'What is the last name of the employee?',
+      },
+      {
+        name: 'roleId',
+        type: 'input',
+        message: 'What role Id number does the employee have?'
+      },
+      {
+        name: 'managerId',
+        type: 'input',
+        message: 'What is the manager ID number for the employee? If none write 0.',
+      }
+    ])
+    .then(function (answer) {
+      connection.query(
+        'INSERT INTO employee SET ?',
+        {
+          first_name: answer.first,
+          last_name: answer.last,
+          role_id: answer.roleId,
+          manager_id: answer.managerId
+        },
+        function (err) {
+          if (err) throw err;
+          console.log('Employee Added!');
+          runPrompt();
+        }
+      );
+    });
+}
+
+
+function viewLog() {
+  inquirer
+    .prompt({
+      name: 'table',
+      type: 'list',
+      message: 'What would you like to review?',
+      choices: ['Department', 'Role', 'Employee']
     })
-  };
+    .then(function (answer) {
+      if (answer.table === 'Department') {
+        viewDepartment();
+      }
+      else if (answer.table === 'Role') {
+        viewRole();
+      }
+      else if (answer.table === 'Employee') {
+        viewEmployee();
+      }
+      else {
+        connection.end();
+      }
+    });
+}
 
-  function viewDepartment() {
-    connection.query('SELECT * FROM department', function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      runPrompt();
-    })
-  };
+function viewRole() {
+  connection.query('SELECT * FROM role', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runPrompt();
+  });
+};
 
-  function viewEmployee() {
-    connection.query('SELECT * FROM employee', function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      runPrompt();
+function viewDepartment() {
+  connection.query('SELECT * FROM department', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runPrompt();
+  })
+};
+
+function viewEmployee() {
+  connection.query('SELECT * FROM employee', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runPrompt();
     })
-  }; 
-  
+};
